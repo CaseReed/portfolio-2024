94b2790720b52212657b0a959c64c65b182baffe
Julien Tavernier <julien.tavernier.pro@gmail.com>
2024-10-15 23:47:08 +0200

    [main] 0.1.1 FIX: just fix label for projects

--- app/page.tsx
+++ app/page.tsx
@@ -270,7 +270,7 @@ export default async function Home() {
         >
           <div className="container px-4 md:px-6 max-w-7xl mx-auto">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-center mb-8">
-              My Projects
+              My Projects (more to come)
             </h2>
             <motion.div
               className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3"
